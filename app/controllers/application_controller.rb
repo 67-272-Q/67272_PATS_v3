0306dc0f82114262b796b055c3e68087fa7f7add
ladyhodhod <ladyhodhod@gmail.com>
2021-03-30 08:39:47 +0300

    added content with authentication and authorization implemented

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,38 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  # # just show a flash message instead of full CanCan exception
+  # rescue_from CanCan::AccessDenied do |exception|
+  #   flash[:error] = "You are not authorized to take this action."
+  #   redirect_to home_path
+  # end
+
+  # handle 404 errors with an exception as well
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    flash[:error] = "Seek and you shall find... but not this time"
+    redirect_to home_path
+  end
+  
+  # Rescue from bad password reset token
+  rescue_from Exceptions::NoPasswordResetToken do |exception|
+    flash[:error] = "The password reset token is either invalid or has expired."
+    redirect_to home_path
+  end
+
+
+  private
+  # Handling authentication
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+  
+  def logged_in?
+    current_user
+  end
+  helper_method :logged_in?
+  
+  def check_login
+    redirect_to login_path, alert: "You need to log in to view this page." if current_user.nil?
+  end
+end
